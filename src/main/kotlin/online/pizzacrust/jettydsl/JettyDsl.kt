31bed120631365e006bd010531eee501631f3646
PizzaCrust <pizzacrustcoder@gmail.com>
2019-06-07 04:15:28 +0700

    ip

--- src/main/kotlin/online/pizzacrust/jettydsl/JettyDsl.kt
+++ src/main/kotlin/online/pizzacrust/jettydsl/JettyDsl.kt
@@ -5,16 +5,19 @@ import org.eclipse.jetty.server.Server
 import org.eclipse.jetty.server.handler.AbstractHandler
 import org.eclipse.jetty.server.handler.ContextHandler
 import org.eclipse.jetty.server.handler.ContextHandlerCollection
+import java.net.InetAddress
 import javax.servlet.http.HttpServletRequest
 import javax.servlet.http.HttpServletResponse
 import javax.servlet.http.HttpServletResponse.SC_OK
 
 data class ServerResponse(val response: Int = SC_OK, val responseString: String? = null)
 
-internal class AbstractHandlerBlock(val block: (Map<String, Array<String>>) -> ServerResponse):
+internal class AbstractHandlerBlock(val block: (Map<String, Array<String>>, InetAddress) ->
+ServerResponse):
         AbstractHandler() {
     override fun handle(target: String?, baseRequest: Request?, request: HttpServletRequest?, response: HttpServletResponse?) {
-        val responseBlock = block(request!!.parameterMap)
+        val responseBlock = block(request!!.parameterMap, InetAddress.getByName(request
+                .remoteAddr))
         response?.status = responseBlock.response
         if (responseBlock.responseString != null) {
             response?.writer?.println(responseBlock.responseString)
@@ -27,7 +30,7 @@ class ServerConstructor {
 
     val paths: MutableList<ContextHandler> = mutableListOf()
 
-    fun path(path: String, block: (Map<String, Array<String>>) -> ServerResponse) {
+    fun path(path: String, block: (Map<String, Array<String>>, InetAddress) -> ServerResponse) {
         val handler = ContextHandler()
         handler.contextPath = path
         handler.handler = AbstractHandlerBlock(block)
